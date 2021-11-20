46ac8656eaa2768c80dc9015e6ba09249a12c17a
Micheal Limones <micheal@zestylimones.com>
2021-11-19 18:31:48 -0700

    added MUI.

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,23 @@
+import React from 'react';
+import AppBar from '@mui/material/AppBar';
+// import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      {/* content goe shere */}
-      <p>test</p>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography mx="auto" variant="h4" component="div">
+            Here are my creations!
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Button variant="contained">Hello World</Button>
     </div>
   );
 }
