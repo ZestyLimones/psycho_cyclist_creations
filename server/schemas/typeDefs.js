29baf616be704c7a23ea9817db6d4488203e1a67
Micheal Limones <micheal@zestylimones.com>
2021-12-02 11:16:57 -0700

    added mutations to typpeDefs.

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -39,6 +39,23 @@ const typeDefs = gql`
     order(_id: ID!): Order
     checkout(items: [ID]!): Checkout
   }
+  type Mutation {
+    addUser(
+      firstName: String!
+      lastName: String!
+      email: String!
+      password: String!
+    ): Auth
+    addOrder(items: [ID]!): Order
+    updateUser(
+      firstName: String
+      lastname: String
+      email: String
+      password: String
+    ): User
+    updateItem(_id: ID!, quantity: Int): Item
+    login(email: String!, password: String!): Auth
+  }
 `;
 
 module.exports = typeDefs;
