bbd5ad4c2e6eea05bc03442d87e2b892ad6172f4
Micheal Limones <micheal@zestylimones.com>
2021-12-06 10:11:37 -0700

    added queries for items and user.

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,1 +1,32 @@
 import { gql } from '@apollo/client';
+
+export const QUERY_USER = gql`
+  query getItems {
+    items {
+      _id
+      name
+      price
+      quantity
+      image
+    }
+  }
+  query user($username: String!) {
+    user(username: $username) {
+      _id
+      firstName
+      lastName
+      username
+      orders {
+        _id
+        purchaseDate
+        items {
+          _id
+          name
+          price
+          quantity
+          image
+        }
+      }
+    }
+  }
+`;
