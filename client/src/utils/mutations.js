ea7dcfc779dc7613edfe053cac9cd85b63cedbf5
Micheal Limones <micheal@zestylimones.com>
2021-12-06 10:36:11 -0700

    added ADD USER mutation.

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -25,3 +25,24 @@ export const ADD_ORDER = gql`
         }
     }
 `;
+
+export const ADD_USER = gql`
+  mutation addUser(
+    $firstName: String!
+    $lastName: String!
+    $email: String!
+    $password: String!
+  ) {
+    addUser(
+      firstName: $firstName
+      lastName: $lastName
+      email: $email
+      password: $password
+    ) {
+      token
+      user {
+        _id
+      }
+    }
+  }
+`;
