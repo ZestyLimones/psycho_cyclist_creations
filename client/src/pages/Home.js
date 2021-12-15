41ecbe0ec273f55f8cc1f53bbe0b4c18ccb862ad
Micheal Limones <micheal@zestylimones.com>
2021-12-14 17:50:34 -0700

    created Cart. Added content to Home page.

---
+++ client/src/pages/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ItemList from '../components/ItemList';
+import Cart from '../components/Cart';
+
+const Home = () => {
+  return (
+    <div className="container">
+      <ItemList />
+      <Cart />
+    </div>
+  );
+};
